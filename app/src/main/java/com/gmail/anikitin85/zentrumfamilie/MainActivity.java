9c5d829e68c2ddf1270ed56f31941bbec77f6796
Alexey Nikitin <anikitin85@gmail.com>
2017-01-21 21:51:11 +0100

    Made address clickable

--- app/src/main/java/com/gmail/anikitin85/zentrumfamilie/MainActivity.java
+++ app/src/main/java/com/gmail/anikitin85/zentrumfamilie/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gmail.anikitin85.zentrumfamilie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openMap(View v) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?q=Eschenheimer Anlage 21, 60318 Frankfurt am Main, Germany"));
+        startActivity(intent);
     }
 }
